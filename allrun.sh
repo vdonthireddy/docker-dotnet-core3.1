7b75ac21e07aca29b1ce87eb16d2a3efe260bde7
Vijaya Donthireddy <donmac@vijayas-mbp.lan>
2020-03-03 22:04:57 -0800

    .

--- allrun.sh
+++ allrun.sh
@@ -14,18 +14,18 @@ RUN dotnet publish -c release -o /output
 FROM mcr.microsoft.com/dotnet/core/aspnet:3.1
 WORKDIR /app
 COPY --from=build /output .
-ENV ASPNETCORE_URLS=http://+:8080
+ENV ASPNETCORE_URLS=http://+:8435
 ENTRYPOINT ["dotnet", "deleteme001.dll"]
 ' > Dockerfile
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi -f deleteme001
 docker build -t deleteme001:1.0 .
-docker run -d -p 8435:8080 --name deleteme001 deleteme001:1.0
+docker run -d -p 8193:8435 --name deleteme001 deleteme001:1.0
 echo '3...'
 sleep 1s
 echo '2...'
 sleep 1s
 echo '1...'
 sleep 1s
-curl http://localhost:8435/weatherforecast
+curl http://localhost:8193/weatherforecast
